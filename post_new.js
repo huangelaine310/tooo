4f6f70e393d8d75de4250078ed66e54710c194ae
huangelaine310 <yh467@cornell.edu>
2021-05-29 16:12:22 +0800

    changed post_new.js to parse messages.json

--- post_new.js
+++ post_new.js
@@ -1,20 +1,67 @@
 console.log('post new js running!');
 const i18n = chrome.i18n;
+const DEFAULT_LOCALE = 'zh_TW';
 
-/** HEADER SECTION */
-// p.required.notice
-$('p.required.notice').html(i18n.getMessage("required"));
+// get lang locale
+let lang = i18n.getUILanguage();
+console.log(lang);
+// change - charcater to _
+lang = lang.replace('-', '_');
 
 
-/** TAGS SECTION (fieldset.work-meta) */
-// header
-$('fieldset.work.meta > legend').html(i18n.getMessage("tags"));
-$('fieldset.work.meta > p.note').html(i18n.getMessage("tags_heading_note"));
+/** parses description field in messages.json to get jquery selector. **/
+function parseDescription(description) {
+  return description.substr(0, description.indexOf('|'));
+}
 
-// rating
-$("label[for='work_rating_string']").html(i18n.getMessage("rating"));
-$("option[value='Not Rated']").html(i18n.getMessage("rating_not_rated"));
-$("option[value='General Audiences']").html(i18n.getMessage("rating_general"));
-$("option[value='Teen And Up Audiences']").html(i18n.getMessage("rating_teen"));
-$("option[value='Mature']").html(i18n.getMessage("rating_mature"));
-$("option[value='Explicit']").html(i18n.getMessage("rating_explicit"));
+/** iterates through messages.json objects and replaces with translation. */
+function iterateMessagesToTranslate(json) {
+  Object.values(json).forEach((value) => {
+    const description = value['description'];
+    const translated_str = value['message'];
+    const selector = parseDescription(description);
+    $(selector).html(translated_str);
+  });
+}
+
+/** fetches the correct messaes.json according to language */
+function fetchJson(tries=0) {
+  console.log('fetch json');
+  if (tries > 1) {
+    console.log('Error: stopping script to avoid stack overflow.');
+    return;
+  }
+  let url = chrome.runtime.getURL('_locales/'+lang+'/messages.json');
+  console.log(url);
+  fetch(url)
+    .then((response) => {
+      response.json().then((json) => {iterateMessagesToTranslate(json);});
+    })
+    .catch((error) => {
+      console.log(error);
+      lang = DEFAULT_LOCALE;
+      fetchJson(tries+1);
+    });
+}
+
+fetchJson();
+
+// /** HEADER SECTION */
+// // p.required.notice
+// $('p.required.notice').html(i18n.getMessage("required"));
+//
+//
+// /** TAGS SECTION (fieldset.work.meta) */
+// // header
+// $('fieldset.work.meta > legend').html(i18n.getMessage("tags"));
+// $('fieldset.work.meta > p.note').html(i18n.getMessage("tags_heading_note"));
+//
+// // rating
+// $("label[for='work_rating_string']").html(i18n.getMessage("rating"));
+// $("option[value='Not Rated']").html(i18n.getMessage("rating_not_rated"));
+// $("option[value='General Audiences']").html(i18n.getMessage("rating_general"));
+// $("option[value='Teen And Up Audiences']").html(i18n.getMessage("rating_teen"));
+// $("option[value='Mature']").html(i18n.getMessage("rating_mature"));
+// $("option[value='Explicit']").html(i18n.getMessage("rating_explicit"));
+//
+// //archive warnings
